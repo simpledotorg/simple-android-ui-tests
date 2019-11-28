0bee6d26c4df5b3c06ff2260c873830c6d62ceaf
prerana <prerana.testvagrant@embibe.com>
2019-11-28 17:35:45 +0530

    Added script for patient  Sync

--- src/test/java/steps/LaunchAppSteps.java
+++ src/test/java/steps/LaunchAppSteps.java
@@ -1,10 +1,17 @@
 package steps;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import pages.LoginPage;
 
-public class LaunchAppSteps extends BaseSteps{
+public class LaunchAppSteps extends BaseSteps {
     @Given("^(\\w+) Launches app$")
     public void launchApp(String User) {
-      getDriverInstanceFor(User);
+        getDriverInstanceFor(User);
+    }
+
+    @And("^(\\w+) resets app$")
+    public void userResetsApp(String User) { new LoginPage(getDriverInstanceFor(User)).resetApp();
     }
 }
