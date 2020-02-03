8da10aaf9442a723946eb9db94bf08de1a9d8698
prerana <prerana.testvagrant@embibe.com>
2020-02-03 09:47:36 +0530

    reduced implicit wait

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -29,7 +29,7 @@ public class BasePage {
 
     public BasePage(AppiumDriver driver) {
         this.driver = driver;
-        this.driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        this.driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         wait = new WebDriverWait(this.driver, 20);
     }
 
